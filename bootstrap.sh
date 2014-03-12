09eade08b9b9a83cd62b22d9aebba627cf8c4bff
cwahbong <cw.ahbong@gmail.com>
2014-03-12 16:31:40 +0800

    Move font config to XDG_CONFIG_HOME.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,45 @@
-#!/bin/bash
+#!/bin/sh -eu
 
-set -e -u
+DOTVIM_ROOT="$HOME/.vim"
+: ${VIMRC_LINK:=$HOME/.vimrc}
+: ${XDG_CONFIG_HOME:=$HOME/.config}
 
-VIMRC_LINK="${HOME}/.vimrc"
+font_setup(){
+	FONT_CONF_D=$XDG_CONFIG_HOME/fontconfig/conf.d
+	if [ ! -d ~/.fonts ]
+	then
+		mkdir ~/.fonts
+	fi
+	cp fonts/*.otf ~/.fonts
+	if [ ! -d $FONT_CONF_D ]
+	then
+		mkdir -p $FONT_CONF_D
+	fi
+	cp fonts/*.conf $FONT_CONF_D
+	fc-cache -vf ~/.fonts
+}
+
+vundle_setup(){
+	if [ ! -d "bundle" ]
+	then
+		mkdir bundle
+	fi
+	cd bundle
+	if [ ! -d "vundle" ]
+	then
+		git clone https://github.com/gmarik/vundle.git
+	else
+		echo "vundle already cloned."
+	fi
+}
+
+plugin_setup(){
+	if [ -d "vundle" ]
+	then
+		vim -c BundleInstall -c qa
+	fi
+}
+
 
 if [ $PWD != "$HOME/.vim" ]
 then
@@ -17,31 +54,6 @@ else
 	ln -s .vim/vimrc $VIMRC_LINK
 fi
 
-# font settings
-if [ ! -d ~/.fonts ]
-then
-	mkdir ~/.fonts
-fi
-cp fonts/*.otf ~/.fonts
-if [ ! -d ~/.fonts.conf.d ]
-then
-	mkdir ~/.fonts.conf.d
-fi
-cp fonts/*.conf ~/.fonts.conf.d
-fc-cache -vf ~/.fonts
-
-if [ ! -d "bundle" ]
-then
-	mkdir bundle
-fi
-cd bundle
-if [ ! -d "vundle" ]
-then
-	git clone https://github.com/gmarik/vundle.git
-else
-	echo "vundle already cloned."
-fi
-if [ -d "vundle" ]
-then
-	vim -c BundleInstall -c qa
-fi
+font_setup
+vundle_setup
+plugin_setup
